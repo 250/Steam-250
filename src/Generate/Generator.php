cde96fd861d4714fdd2fc5fa3e67cf858460ba8e
Bilge <bilge@scriptfusion.com>
2017-11-01 00:25:34 +0000

    Added decorator to add missing app details such as app type.

--- src/Generate/Generator.php
+++ src/Generate/Generator.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace ScriptFUSION\Steam250\Generate;
 
 use Doctrine\DBAL\Connection;
+use ScriptFUSION\Steam250\Queries;
 
 class Generator
 {
@@ -19,18 +20,7 @@ class Generator
 
     public function generate(): void
     {
-        $cursor = $this->database->executeQuery(
-            'SELECT *,
-                (
-                    (positive_reviews + 1.9208) / total_reviews - 1.96
-                        * SQRT((positive_reviews * negative_reviews) / total_reviews + 0.9604)
-                        / total_reviews
-                ) / (1 + 3.8416 / total_reviews) AS score
-             FROM review
-             ORDER BY score DESC
-             LIMIT 250'
-        );
-
+        $cursor = Queries::fetchTop250Games($this->database);
         $games = $cursor->fetchAll();
 
         file_put_contents('site/250.html', $this->twig->load('250.twig')->render(compact('games')));
