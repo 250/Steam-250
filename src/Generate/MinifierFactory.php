0bb5120f4fa245862a168c8aa1a4dfb3bc721277
Bilge <bilge@scriptfusion.com>
2017-12-05 23:27:20 +0000

    Updated HtmlMin to version 2, saves additional ~8-9%.

--- src/Generate/MinifierFactory.php
+++ src/Generate/MinifierFactory.php
@@ -11,9 +11,6 @@ final class MinifierFactory
     {
         $minifier = new HtmlMin;
 
-        // This is invalid. See: https://github.com/voku/HtmlMin/issues/6.
-        $minifier->doRemoveHttpPrefixFromAttributes(false);
-
         // This is buggy. See: https://github.com/voku/HtmlMin/issues/9. Only saves 22 bytes anyway!
         $minifier->doRemoveWhitespaceAroundTags(false);
 
