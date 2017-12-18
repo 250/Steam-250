161a0c99178be3e560079326ef765389daeeedba
Bilge <bilge@scriptfusion.com>
2017-12-18 14:58:23 +0000

    Updated screenshot script to hide unwanted elements.

--- extra/screenshot.js
+++ extra/screenshot.js
@@ -11,10 +11,20 @@ const
 
 let webpage = require('webpage').create();
 
-webpage.viewportSize = {width: 1000, height: screen.height};
+webpage.viewportSize = {width: 1160, height: screen.height};
 
 (async () => {
     for (let page of PAGES) {
-        await webpage.open(BASE_URL + page, () => webpage.render(`${page}.png`));
+        await webpage.open(BASE_URL + page, () => {
+            webpage.evaluate(() => {
+                // Remove ads.
+                document.querySelectorAll('ins').forEach(e => e.remove());
+
+                // Hide fixed links.
+                document.querySelector('.fixedlinks').style.display = 'none';
+            });
+
+            webpage.render(`${page}.png`);
+        });
     }
 })().then(() => slimer.exit());
