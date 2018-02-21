f7464742bd516edc1bc0aa07528f1d134116859f
Bilge <bilge@scriptfusion.com>
2018-02-21 12:42:36 +0000

    Fixed issue causing week top to be locked to Monday.
    Due to a quirk in PHP's "last week" relative period, the "rolling week"
    was actually hard locked to Monday of the previous week instead of seven
    days ago.

--- src/Ranking/Impl/ThisWeekRanking.php
+++ src/Ranking/Impl/ThisWeekRanking.php
@@ -9,7 +9,7 @@ class ThisWeekRanking extends RollingRanking
 {
     public function __construct(RankingDependencies $dependencies)
     {
-        parent::__construct($dependencies, '7day', 'last week', 20);
+        parent::__construct($dependencies, '7day', '-7 day', 20);
 
         $this->setWeight(4);
     }
