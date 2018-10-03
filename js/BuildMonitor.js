0f348f6d77491cc19287256144a90c0ebff0a23c
Travis <bilge+travis@scriptfusion.com>
2018-10-03 18:14:46 +0000

    Built from d4a401736cb2014c95f5e517054583619563c851.

--- js/BuildMonitor.js
+++ js/BuildMonitor.js
@@ -19,7 +19,7 @@ class BuildMonitor {
     }
 
     static createElement() {
-        let element = document.createElement('div');
+        return element = document.createElement('div');
         element.classList.add('countdown');
         element.innerHTML = 'Initializing...';
 
