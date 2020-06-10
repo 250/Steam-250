e8423353c0ddf79ced84e0d588dc4fedf157f60b
Travis <bilge+travis@scriptfusion.com>
2020-06-10 22:24:45 +0000

    Built from 62402e22af2e1d79304ff6d906c34cec098f82e0.

--- js/250.js
+++ js/250.js
@@ -26,11 +26,6 @@ class S250 {
     initLogInOut() {
         const form = document.querySelector('#lout form');
 
-        if (!form) {
-            console.debug('Steam user area unavailable: skipped.');
-            return;
-        }
-
         // Redirect back to same page without query or hash.
         form['openid.return_to'].value = location.origin + location.pathname;
 
@@ -366,11 +361,6 @@ class S250 {
     }
 
     initCountdown() {
-        if (typeof BuildMonitor !== 'function') {
-            console.debug('BuildMonitor unavailable: skipping.')
-            return;
-        }
-
         const element = BuildMonitor.createElement();
 
         fetch(
