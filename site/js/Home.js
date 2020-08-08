cd4580b5063e48dab781e8a533a669091b9ca3a6
Bilge <bilge@scriptfusion.com>
2020-08-08 22:19:38 +0100

    Finalized R&F compact ranking presentation. Readded not-allowed cursor for video missing banners in compact ranking. Added some presentation for Club 250 R&F section.

--- site/js/Home.js
+++ site/js/Home.js
@@ -1,28 +1,6 @@
 new class {
     constructor() {
-        this.initRankingHoverItems();
         this.initChevrons();
-    }
-
-    initRankingHoverItems() {
-        document.querySelectorAll('.compact.ranking li > .title').forEach(a => {
-            const shadow = a.appendChild(a.cloneNode(true));
-
-            // Prevent rapid re-entry when shape is clipped.
-            shadow.style.pointerEvents = 'none';
-
-            a.addEventListener('mouseenter', _ => {
-                // Cancel any currently running animation on re-entry.
-                shadow.classList.remove('animate');
-                // Reflow hack to force immediate application.
-                shadow.offsetWidth;
-
-                shadow.classList.add('animate');
-            })
-            a.addEventListener('animationend', _ => {
-                shadow.classList.remove('animate');
-            })
-        });
     }
 
     // This is just to save generating superfluous markup.
