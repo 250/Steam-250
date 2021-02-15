e919fe9ce87f5fdc898c671c0075e2160be6c92f
Bilge <bilge@scriptfusion.com>
2021-02-15 12:50:28 +0000

    Moved static assets from /site to /assets.
    This change allows us to completely ignore /site in git and in the
    editor, creating better separation of concerns and cleaning up the
    editor search path. webpack now copies the static assets into /site
    automatically.

--- webpack.config.js
+++ webpack.config.js
@@ -3,25 +3,42 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     mode: 'development',
+    context: path.resolve(__dirname, 'assets'),
     entry: {
         250: [
-            './site/js/250.js',
-            './site/js/VideoPlayer.js',
+            './js/250.js',
+            './js/VideoPlayer.js',
 
-            './site/css/inc/reset.css',
-            './site/css/inc/250.css',
-        ]
+            './css/inc/reset.css',
+            './css/inc/250.css',
+        ],
     },
 
     output: {
-        path: path.resolve(__dirname, 'site/c')
+        path: path.resolve(__dirname, 'site/c'),
     },
 
     plugins: [
-        new MiniCssExtractPlugin()
+        new MiniCssExtractPlugin(),
+        new CopyPlugin({
+            patterns: [
+                // TODO: Exclude JS and CSS when they're fully compiled.
+                { from: '.', to: '..' },
+            ],
+        }),
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns: [
+                // Remove all files containing a '.' (to distinguish between files and directories).
+                path.join(process.cwd(), 'site/**/*.*'),
+                // Do not remove HTML files.
+                '!' + path.join(process.cwd(), 'site/**/*.html'),
+            ],
+        }),
     ],
 
     resolve: {
@@ -38,14 +55,14 @@ module.exports = {
                 test: /\.(less|css)$/,
                 use: [
                     {
-                        loader: MiniCssExtractPlugin.loader
+                        loader: MiniCssExtractPlugin.loader,
                     },
                     {
                         loader: 'css-loader',
                         options: {
                             url: false,
                             sourceMap: true,
-                        }
+                        },
                     },
                     {
                         loader: 'less-loader',
@@ -54,11 +71,11 @@ module.exports = {
                             lessOptions: {
                                 strictUnits: true,
                             },
-                        }
-                    }
-                ]
-            }
-        ]
+                        },
+                    },
+                ],
+            },
+        ],
     },
 
     optimization: {
