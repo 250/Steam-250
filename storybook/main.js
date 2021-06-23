c02efc11f4e20dc30b0b49e132a9e210850a057f
Bilge <bilge@scriptfusion.com>
2021-06-23 21:40:17 +0100

    Moved all Storybook files under /storybook top level directory.

--- storybook/main.js
+++ storybook/main.js
@@ -1,7 +1,7 @@
 module.exports = {
   stories: [
-    '../src/**/*.stories.mdx',
-    '../src/**/*.stories.@(js|jsx|ts|tsx)'
+    './stories/*.stories.mdx',
+    './stories/*.stories.@(js|jsx|ts|tsx)'
   ],
   addons: [
     '@storybook/addon-links',
