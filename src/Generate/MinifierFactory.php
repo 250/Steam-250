64dc402abd3b3f6f33966662ebfcd607a40b1603
Bilge <bilge@scriptfusion.com>
2023-01-23 12:27:52 +0000

    Upgraded HtmlMin and Symfony Console.

--- src/Generate/MinifierFactory.php
+++ src/Generate/MinifierFactory.php
@@ -9,11 +9,6 @@ final class MinifierFactory
 {
     public function create(): HtmlMin
     {
-        $minifier = new HtmlMin;
-
-        // This is buggy. See: https://github.com/voku/HtmlMin/issues/9. Only saves 22 bytes anyway!
-        $minifier->doRemoveWhitespaceAroundTags(false);
-
-        return $minifier;
+        return new HtmlMin;
     }
 }
